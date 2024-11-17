bce24d3a38a4235a8c8be5b5d6b8a15459906249
Paul Schmitt <paul.schmitt456@gmail.com>
2024-11-17 18:41:13 +0100

    update the api + display data from the api

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([])
+
+
+  
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/books/');
+      setBooks(response.data);
+      console.log(response.data)
+    } catch (err) {
+      console.log("error");
+    } 
+  };
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
 
   return (
     <>
@@ -14,8 +34,16 @@ function App() {
         <input type="text" placeholder='Name of the book'/>
         <input type="text" placeholder='Date of release'/>
         <input type="text" placeholder='Author'/>
-        <button>submit</button>
+        <button onClick={() => console.log(books)}>submit</button>
       </div>
+      {books.map((book) => 
+        <div>
+          <h1>{book.title}</h1>
+          <p>{book.date}</p>
+          <p>{book.author}</p>
+        </div>
+      )}
+
     </>
   )
 }
--- server/myserver/api/views.py
+++ server/myserver/api/views.py
@@ -13,3 +13,12 @@ def get_books(request):
     books = Book.objects.all()
     serializedData = BookSerializer(books, many = True).data
     return Response(serializedData)
+
+@api_view(['POST'])
+def create_books(request):
+    data = request.data
+    serializer = BookSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
